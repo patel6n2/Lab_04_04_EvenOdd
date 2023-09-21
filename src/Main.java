66b7e5303f7534ddd15e27686bcb026911608dd2
Nishaan Patel <patel6n2@mail.uc.edu>
2023-09-21 00:33:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int result = 0;
+        int numToExamine = 2;
+        result = numToExamine % 2;
+        System.out.println("If the result is 0, your number is even. If the result is 1, your number is odd.");
+        System.out.println("The result is: " + result);
+    }
+}
